cad0a710a58e9e5da14a4fd0fda7e5f0e34d4500
Mathias Jahnke <mjahnke@outlook.com>
2016-03-29 13:43:29 +0200

    changes

--- ProcessingUIComponents/src/mj/processing/button/PButton.java
+++ ProcessingUIComponents/src/mj/processing/button/PButton.java
@@ -1,5 +1,13 @@
 package mj.processing.button;
 
+/**
+ * A button class to use within <a href="http://www.processing.org">processing</a>.
+ * Action handling should by done via processings mouseClicked() functions etc. <br>
+ * 
+ * @author Mathias Jahnke, Technische Universit&auml;t M&uuml;nchen, <a href="http://www.lfk.bgu.tum.de">Chair of Cartography</a>
+ * @version 0.0.2
+ * @since 12.06.2014
+ */
 public class PButton {
 
 }
